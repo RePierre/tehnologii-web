1221e5aed71bbc5605a9aa46e9a709a851cf1208
RePierre <petru.rebeja@gmail.com>
2025-04-09 13:34:27 +0300

    Added exercise files

---
+++ e03/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ro">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oale și ulcele</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Pagina principală</a></li>
+                <li><a href="#">Despre noi</a></li>
+                <li><a href="#">Servicii</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+     <!-- Cerințe: -->
+     <!-- 1. Creați un formular de feedback -->
+     <!-- 2. Implementați logica de validare și afișați erorile -->
+     <!-- 3. Redirecționați răspunsul la pagina de start -->
+    </main>
+    <footer>
+        <p>Contact us at: deschide@oalesiulcele.ro</p>
+    </footer>
+</body>
+</html>
---
+++ e03/register-feedback.php
@@ -0,0 +1,3 @@
+<?php
+echo "Implement me..."
+?>
